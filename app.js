dcd1c6124f960aca4f3c0da45e8c44209864f6cc
unknown <herohrx1125@gmail.com>
2020-03-20 00:55:30 +0700

    aiml test

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const AIMLParser = require('aimlparser')
+
 const app = express()
 const port = process.env.PORT || 4000
+const aimlParser = new AIMLParser({ name:'HelloBot' })
+
+aimlParser.load(['./test-aiml.xml'])
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -10,15 +15,20 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
+    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+        reply(reply_token, answer)
+    })
     res.sendStatus(200)
 })
+
+app.listen(port)
 
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {CAJK8jG6qWU/BlKoE4RUyYZAa8q0muS53Wp/mDFoOhLv0NwCzx/drlk4Fi8Ltcs+wRHlGayfH2l/bm1ljezXfzwO2dN4vXE6whC7kSnHktJlM2k2m9Plzydp2Qg9ksGloO1bYehEq4wVoCavQ4CJtwdB04t89/1O/w1cDnyilFU=}'
+        'Authorization': 'Bearer {xxxxxxx}'
     }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -26,6 +36,7 @@ function reply(reply_token, msg) {
             text: msg
         }]
     })
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
@@ -34,7 +45,3 @@ function reply(reply_token, msg) {
         console.log('status = ' + res.statusCode);
     });
 }
-
-app.listen(port, () => {
-    console.log(`msging-api\'server is running on localhost:${port}`)
-})
