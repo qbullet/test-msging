bcfd34e3ee669549a2955ceb7717b1c1b6f03c6c
unknown <herohrx1125@gmail.com>
2020-03-20 00:18:32 +0700

    make it better

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+
+app.get('/', (req, res) => {
+    res.send({
+        ok: "true"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`msging-api\'server is running on localhost:${port}`)
+})
